fe60eec54060e5224202955e54399eb1349feadc
nsommerf <nsommerf@gmail.com>
2021-06-05 12:40:47 -0500

    added best trained model

--- app.py
+++ app.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b5b69e06c841a37ea61e0c938fe7acba6013342422aa38171feda612739e2ec9
-size 1321
+oid sha256:ace2d5f7b73286af008f09fcb80f85c8bf411823ffe8a1a1a52d79f05537f75c
+size 1016
