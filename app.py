2ab4b41f60c3f2192915d0b00566dae8e725f40d
nsommerf <nsommerf@gmail.com>
2021-06-05 12:45:59 -0500

    one last clean up

--- app.py
+++ app.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5cdde220df5b800569dd5793d3a39d1dc98297e66702aec875346c2fec64ecc8
-size 1073
+oid sha256:30d5708058494bdd6a8cc76ed30f824b9171e9557ebfc69d37a00d5848d4cb45
+size 1057
