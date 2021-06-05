a34a3eae0d94901bc4d2b73435dfadc2aa7d50bd
nsommerf <nsommerf@gmail.com>
2021-06-05 12:44:42 -0500

    added best trained model coeff

--- app.py
+++ app.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ace2d5f7b73286af008f09fcb80f85c8bf411823ffe8a1a1a52d79f05537f75c
-size 1016
+oid sha256:5cdde220df5b800569dd5793d3a39d1dc98297e66702aec875346c2fec64ecc8
+size 1073
